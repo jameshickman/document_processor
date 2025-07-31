731edfa0d6fb8278a5efe73ac1988a0fd54a6738
telendry <james@ecorealsolutions.com>
2025-07-30 20:44:45 -0700

    Test web-hook listener

---
+++ testing/webhook_tester.py
@@ -0,0 +1,32 @@
+import logging
+import os
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+
+
+app = FastAPI()
+
+class ExtractionPayload(BaseModel):
+    result: dict
+    file_name: str
+    document_id: int
+    csf_token: str
+
+
+@app.post('/webhook')
+async def webhook(payload: ExtractionPayload):
+    print(payload)
+    logging.info(f"Webhook received: {payload}")
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "webhook_tester:app",
+        host=os.environ.get("HOST", "0.0.0.0"),
+        port=os.environ.get("PORT", "8001"),
+        reload=os.environ.get("DEBUG", "false").lower() == "true"
+    )
